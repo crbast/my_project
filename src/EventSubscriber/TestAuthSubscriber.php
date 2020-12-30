3ddf19457b5a2838b9f09a65ba08ae1d6a3802c3
crbast <bastien.crettenand@hotmail.com>
2020-12-30 20:20:43 +0000

    Add basic event subscriber

---
+++ src/EventSubscriber/TestAuthSubscriber.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\EventSubscriber;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Event\ControllerEvent;
+use Symfony\Component\HttpKernel\Event\RequestEvent;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+class TestAuthSubscriber implements EventSubscriberInterface
+{
+    public function onKernelRequest(RequestEvent $event)
+    {
+        if (!$event->getRequest()->query->get('token')) {
+            $response = new JsonResponse();
+            $response->setContent(json_encode(
+                ['success' => false, 'data' => 'Bad token provided']
+            ));
+            $event->setResponse($response);
+        }
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            KernelEvents::REQUEST => 'onKernelRequest',
+        ];
+    }
+}
