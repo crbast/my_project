4c42dcd5db97aa4501ddc1ccfc509102bce09550
crbast <bastien.crettenand@hotmail.com>
2021-01-02 12:43:58 +0000

    Add debug

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -10,6 +10,9 @@ class UserController extends AbstractController
     public function index(): JsonResponse
     {
         $jsonobj = '{"Peter":35,"Ben":37,"Joe":43}';
+        $temp2 = ['hello' => 'helo'];
+        $temp2[1] = '';
+
         return new JsonResponse(json_decode($jsonobj));
     }
 }
--- src/EventSubscriber/TestAuthSubscriber.php
+++ src/EventSubscriber/TestAuthSubscriber.php
@@ -4,10 +4,7 @@ namespace App\EventSubscriber;
 
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\Event\ControllerEvent;
 use Symfony\Component\HttpKernel\Event\RequestEvent;
-use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 use Symfony\Component\HttpKernel\KernelEvents;
 
 class TestAuthSubscriber implements EventSubscriberInterface
