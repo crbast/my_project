68d9cdf617aef53b0d03dfd095ed71e54629ee41
crbast <bastien.crettenand@hotmail.com>
2021-01-03 09:22:35 +0000

    add POST PostController

--- config/routes.php
+++ config/routes.php
@@ -1,10 +1,22 @@
 <?php
 
+use App\Controller\PostController;
 use App\Controller\UserController;
 use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
 
 return function (RoutingConfigurator $routes) {
-  $routes->add('index', '/')
-    ->controller([UserController::class, 'index'])
-    ->methods(['GET', 'HEAD']);
+    $routes->add('index', '/')
+        ->controller([UserController::class, 'index'])
+        ->methods(['GET', 'HEAD'])
+    ;
+
+    $routes->add('post:index', '/post')
+        ->controller([PostController::class, 'index'])
+        ->methods(['GET'])
+    ;
+
+    $routes->add('post:create', '/post')
+        ->controller([PostController::class, 'create'])
+        ->methods(['POST'])
+    ;
 };
--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -6,21 +6,31 @@ use App\Entity\Post;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\Encoder\JsonEncoder;
 use Symfony\Component\Serializer\SerializerInterface;
 
 class PostController extends AbstractController
 {
-    /**
-     * @Route("/post", name="post")
-     */
     public function index(SerializerInterface $serializer, EntityManagerInterface $entityManager): JsonResponse
     {
         $temp = $entityManager->getRepository(Post::class)->findAll();
         $data = $serializer->serialize($temp, JsonEncoder::FORMAT);
 
         return new JsonResponse($data, Response::HTTP_OK, [], true);
+    }
+
+    public function create(SerializerInterface $serializer, EntityManagerInterface $entityManager, Request $request): JsonResponse
+    {
+        $post = new Post();
+        $post->setTitle($request->get('title'));
+        $post->setUser($request->get('user'));
+        $post->setContent($request->get('content'));
+
+        $entityManager->persist($post);
+        $entityManager->flush();
+
+        return new JsonResponse($serializer->serialize($post, JsonEncoder::FORMAT), Response::HTTP_OK, [], true);
     }
 }
